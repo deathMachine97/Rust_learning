fc6ae757806b85e65a14303f560088c8f28143b4
mukhtar_abeshov <mukhtarabeshov@gmail.com>
2019-07-30 21:28:38 +0600

    Enter

--- test_cargo/src/main.rs
+++ test_cargo/src/main.rs
@@ -1,10 +1,14 @@
 use std::io;
 fn main() {
+	println!("Для выхода введите 'exit'");
+	println!("Введите число");
 	loop{
-		println!("Guess the number");
 		let mut guess = String::new();
 		io::stdin().read_line(&mut guess)
 			.expect("This must be str");
+		if guess.trim() == "exit".to_string(){
+			break;
+		}
 		let i32_guess:i32 = match guess.trim().parse::<i32>(){
 			Ok(num)=> num,
 			Err(_)=>{
@@ -12,6 +16,6 @@ fn main() {
 				continue;
 			}
 		};
-		println!("The nubmer is {}",i32_guess);
+		println!("Ваше число - {}. Введите еще одно число",i32_guess);
 	}
 }
